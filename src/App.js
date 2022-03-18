8d5c22114276567add3c8466c1f3a3d3a633e317
Laya Bingi <bingilaya472@gmail.com>
2022-03-18 14:57:12 +0000

    Click Counter App Solution

---
+++ src/App.js
@@ -0,0 +1,9 @@
+// Published URL ==>  https://saiclickcntrweb.ccbp.tech/
+
+import ClickCounter from './components/ClickCounter'
+
+import './App.css'
+
+const App = () => <ClickCounter />
+
+export default App
